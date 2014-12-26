82c01577b91ca41867d48849cc5292633027abe3
captucaptu <captucaptu@gmail.com>
2014-12-26 13:51:01 +0800

    update towel book

--- old/function.js
+++ old/function.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-	//docs.google.com/spreadsheets/d/12fM7M2yrhFMHQedH-npFE3A7BZkd_xX9wbIpmfFi4VY/edit?usp=sharing
-	var key = '12fM7M2yrhFMHQedH-npFE3A7BZkd_xX9wbIpmfFi4VY';
+	var key = '1aCJdNxlGT2VqckNxuxWWJPXNvNrb3SGgGuqmCN8ty0Y';
 	var userURL = 'https://spreadsheets.google.com/feeds/list/' + key + '/1/public/values?alt=json';
 	
 	queryTotal();
@@ -56,26 +55,27 @@ $(document).ready(function(){
 				});
 				var find = false;
 				for (var k = 0; k<bookers.length; k++) {
-					if(bookers[k][1]==pttid && (bookers[k][7]==(tel.substring(1,tel.length)) || bookers[k][7] == tel)){
+					if(bookers[k][1]==pttid && (bookers[k][9]==(tel.substring(1,tel.length)) || bookers[k][9] == tel)){
 						booker = bookers[k];
 						$('li.pttid').text("PTT 帳號："+booker[1]);
 						$('li.name').text("訂購人："+booker[2]);
 						$('li.num').text("數量："+booker[3]);
 						$('li.price').text("金額："+booker[4]);
 						$('li.ac').text("對帳資料："+booker[5]);
-						$('li.receive').text("收件人："+booker[6]);
-						if(bookers[k][7]==(tel.substring(1,tel.length))) $('li.tel').text("聯絡電話：0"+booker[7]);
-						if(bookers[k][7] == tel) $('li.tel').text("聯絡電話："+booker[7]);
-						$('li.adds').text("收件地址："+booker[8]);
-						$('li.post').text("取貨方式："+booker[10]);
+						$('li.poster').text("海報寄送："+booker[6]);
+						$('li.change').text("餘款處理："+booker[7]);
+						$('li.receive').text("收件人："+booker[8]);
+						if(bookers[k][9]==(tel.substring(1,tel.length))) $('li.tel').text("聯絡電話：0"+booker[9]);
+						if(bookers[k][9] == tel) $('li.tel').text("聯絡電話："+booker[9]);
+						$('li.adds').text("收件地址："+booker[10]);
 						//console.log(booker[12]);
-						if(booker[13]=="TRUE"){
+						if(booker[12]=="TRUE"){
 							$('li.check').text("處理狀況：匯款已確認收到。");
 						}else{
 							$('li.check').text("處理狀況：已填單，待確認款項與資料。");
 						}
-						//$('li.trackno-1').text("郵件編號："+booker[14]);
-						//$('li.trackno-2').text("郵件編號(海報)："+booker[14]);
+						$('li.trackno-1').text("郵件編號："+booker[13]);
+						$('li.trackno-2').text("郵件編號(海報)："+booker[14]);
 						//$('li.check').text("PTT 帳號："+booker[1]);
 						find = true;
 						break;
