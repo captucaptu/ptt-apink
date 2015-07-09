01815e5a5c2124a8394908bc061d733c06eb5986
captucaptu <captucaptu@gmail.com>
2015-07-10 03:13:38 +0800

    fix bug

--- js/cal-function.js
+++ js/cal-function.js
@@ -27,20 +27,40 @@ $(document).ready(function(){
 			wrap = wrap * num;
 		}
 
-		if(cargo=='false') ship = 0;
-		
 		var tempPost = Math.ceil(num/3);
+
+		if(cargo=='false') {
+			wrap = 0;
+			ship = 0;
+			tempPost = 0;
+		}
+		
+		
 		poster = poster * tempPost;
 		posterShip = posterShip * tempPost;
+
+
 
 
 		var total = cd + ship + wrap + poster + posterShip;
 
 
 		$('.cd').text('您欲訂購 ' + num + ' 張專輯，每張 513 元，小計 ' + cd + ' 元');
-		$('.wrap').text('專輯每張酌收 3 元包材(含氣泡袋、膠帶等等)，小計 ' + wrap + ' 元');
-		$('.ship').text('您欲訂購 ' + num + ' 張專輯，專輯運費為 ' + ship + ' 元');
-		$('.poster').html('海報一共 ' + num + ' 張，海報筒每支 13 元，每支運費 40 元<br>需要海報筒 ' + tempPost + ' 支 '+ poster +' 元，運費 ' + posterShip + ' 元，小計 ' + (poster + posterShip) + ' 元');
+
+		if(cargo=='true'){
+			$('.wrap').show();
+			$('.ship').show();
+			$('.poster').show();
+			$('.wrap').text('專輯每張酌收 3 元包材(含氣泡袋、膠帶等等)，小計 ' + wrap + ' 元');
+			$('.ship').text('您欲訂購 ' + num + ' 張專輯，專輯運費為 ' + ship + ' 元');
+			$('.poster').html('海報一共 ' + num + ' 張，海報筒每支 13 元，每支運費 40 元<br>需要海報筒 ' + tempPost + ' 支 '+ poster +' 元，運費 ' + posterShip + ' 元，小計 ' + (poster + posterShip) + ' 元');
+		}else if(cargo=='false'){
+			$('.wrap').hide();
+			$('.ship').hide();
+			$('.poster').hide();
+		}
+
+		
 		$('.total').text('合計： '+ total +' 元');
 
 
